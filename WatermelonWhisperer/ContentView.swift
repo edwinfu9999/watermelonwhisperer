f071fc268a3c1b79a802fda58e5b9c26f8ab801d
Edwin <Edwinfu9999@gmail.com>
2026-07-17 21:38:12 +0800

    Initial Commit

---
+++ WatermelonWhisperer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatermelonWhisperer
+//
+//  Created by Fu Yi Zhuo on 17/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatermelonWhisperer/WatermelonWhispererApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatermelonWhispererApp.swift
+//  WatermelonWhisperer
+//
+//  Created by Fu Yi Zhuo on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct WatermelonWhispererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
